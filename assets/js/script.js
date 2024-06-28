7edde2989eb6c611a1a88ef724a13ef4e2fb19d2
Neoscripter <sange0337@gmail.com>
2024-06-28 19:42:47 +0800

    Finished the header and banner in the main page

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,28 +4,26 @@ document.addEventListener("DOMContentLoaded", () => {
         const closeBurgerMenuBtn = document.querySelector(".close-burger-menu");
         const popupMenu = document.querySelector(".popup-menu");
         const popupMenuOverlay = document.querySelector(".popup-menu-overlay");
-/*         const headerBtnGroup = document.querySelector(".header-btn-group"); */
-        const primaryLogo = document.querySelector(".primary-logo");
+        const headerBtnGroup = document.querySelector(".header-main__btn-group");
+        const primaryLogo = document.querySelector(".header-main-logo");
 
         burgerMenuBtn.addEventListener("click", () => {
             requestAnimationFrame(() => {
-             /*    headerBtnGroup.style.opacity = "0"; */
-                /* primaryLogo.style.opacity = "0"; */
-                setTimeout(() => {
-                    popupMenuOverlay.style.display = "block";
+                headerBtnGroup.style.opacity = "0";
+                primaryLogo.style.opacity = "0";
+                popupMenuOverlay.style.transform = "translateX(0%)";
                     popupMenu.style.transform = "translateX(0%)";
-                }, 500);
             });
         });
 
         closeBurgerMenuBtn.addEventListener("click", () => {
             requestAnimationFrame(() => {
-           /*      headerBtnGroup.style.opacity = "1"; */
+                popupMenu.style.transform = "translateX(100%)";
+                headerBtnGroup.style.opacity = "1";
+                primaryLogo.style.opacity = "1";
                 setTimeout(() => {
-                    popupMenuOverlay.style.display = "none";
-                    popupMenu.style.transform = "translateX(100%)";
-                }, 500);
-         /*        primaryLogo.style.opacity = "1"; */
+                    popupMenuOverlay.style.transform = "translateX(100%)";
+                }, 1000);
             });
 
         });
