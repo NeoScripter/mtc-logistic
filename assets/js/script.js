2f6efa96c9878843b2e294e0cf7ceaaf072f6fdb
Neoscripter <sange0337@gmail.com>
2024-06-28 18:50:49 +0800

    Finished the header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
     function initBurgerMenu() {
-        const burgerMenuBtn = document.querySelector(".burger-menu");
+        const burgerMenuBtn = document.querySelector(".header-main-burger");
         const closeBurgerMenuBtn = document.querySelector(".close-burger-menu");
-        const popupMenu = document.querySelector(".popup-menu-overlay");
-        const headerBtnGroup = document.querySelector(".header-btn-group");
+        const popupMenu = document.querySelector(".popup-menu");
+        const popupMenuOverlay = document.querySelector(".popup-menu-overlay");
+/*         const headerBtnGroup = document.querySelector(".header-btn-group"); */
         const primaryLogo = document.querySelector(".primary-logo");
 
         burgerMenuBtn.addEventListener("click", () => {
             requestAnimationFrame(() => {
-                popupMenu.style.transform = "translateX(0%)";
-                headerBtnGroup.style.opacity = "0";
-                primaryLogo.style.opacity = "0";
+             /*    headerBtnGroup.style.opacity = "0"; */
+                /* primaryLogo.style.opacity = "0"; */
+                setTimeout(() => {
+                    popupMenuOverlay.style.display = "block";
+                    popupMenu.style.transform = "translateX(0%)";
+                }, 500);
             });
         });
 
         closeBurgerMenuBtn.addEventListener("click", () => {
             requestAnimationFrame(() => {
-                popupMenu.style.transform = "translateX(100%)";
-                headerBtnGroup.style.opacity = "1";
-                primaryLogo.style.opacity = "1";
+           /*      headerBtnGroup.style.opacity = "1"; */
+                setTimeout(() => {
+                    popupMenuOverlay.style.display = "none";
+                    popupMenu.style.transform = "translateX(100%)";
+                }, 500);
+         /*        primaryLogo.style.opacity = "1"; */
             });
+
         });
     }
-    /* initBurgerMenu(); */
+    initBurgerMenu();
 
 });
--- front-page.php
+++ front-page.php
@@ -1,5 +1,29 @@
 <?php get_header(); ?>
-
+<div class="popup-menu-overlay">
+    <div class="popup-menu">
+        <div class="popup-menu__logo-wrapper">
+            <img src="<?php echo get_template_directory_uri() .
+                "/assets/images/logo-black.png"; ?>" alt="logo" class="popup-logo">
+            <img src="<?php echo get_template_directory_uri() .
+                "/assets/images/svgs/close.svg"; ?>" alt="burger-menu" class="close-burger-menu">
+        </div>
+        <?php
+            wp_nav_menu(
+                array(
+                    'theme_location' => 'top-menu',
+                    'container'      => 'nav',
+                    'container_class' => 'nav-popup',
+                    'menu_class'     => 'nav-popup__ul',
+                    'fallback_cb'    => false,
+                )
+            );
+            ?>
+        <div class="popup-btn-group">
+            <button class="popup-btn animated-btn-3">Рассчитать стоимость</button>
+            <button class="popup-btn animated-btn-2">Бесплатный звонок</button>
+        </div>
+    </div>
+</div>
 <main class="home-main centering">
     <section class="home-banner">
             <div class="home-banner__content">
