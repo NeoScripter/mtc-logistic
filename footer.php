e8605836c3147e2bfe9f5ce27fffd4061420890e
Neoscripter <sange0337@gmail.com>
2024-06-30 17:40:44 +0800

    Fininshed the footer

--- footer.php
+++ footer.php
@@ -1,5 +1,60 @@
-<footer class="footer-primary">
-    
+<footer class="footer-primary centering">
+    <div class="footer-top-flex">
+        <div class="footer__flex-top-col-1">
+            <h3>Карта сайта</h3>
+            <?php
+            wp_nav_menu(
+                array(
+                    'theme_location' => 'footer-menu',
+                    'container'      => 'nav',
+                    'container_class' => 'nav-footer',
+                    'menu_class'     => 'nav-footer__ul',
+                    'fallback_cb'    => false,
+                )
+            );
+            ?>
+        </div>
+        <div class="footer__flex-top-col-2">
+            <h3>Услуги</h3>
+            <?php
+            wp_nav_menu(
+                array(
+                    'theme_location' => 'footer-menu-services',
+                    'container'      => 'nav',
+                    'container_class' => 'nav-services',
+                    'menu_class'     => 'nav-services__ul',
+                    'fallback_cb'    => false,
+                )
+            );
+            ?>
+        </div>
+        <div class="footer__flex-top-col-3">
+            <h3>Контакты</h3>
+            <div class="footer__flex-contacts">
+                <div class="footer__links-wrapper">
+                    <p>+7 499 113-55-59</p>
+                    <p>info@mtc-logistic.com</p>
+                    <div class="footer__social-links">
+                        <a href="" class="footer__social-link">
+                            <?php include get_template_directory() . '/assets/images/svgs/telegram.svg';?>
+                        </a>
+                        <a href="" class="footer__social-link">
+                            <?php include get_template_directory() . '/assets/images/svgs/whatsapp.svg';?>
+                        </a>
+                        <a href="" class="footer__social-link">
+                            <?php include get_template_directory() . '/assets/images/svgs/vk.svg';?>
+                        </a>
+                    </div>
+                </div>
+                <button class="footer__calc-btn">Рассчитать стоимость</button>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <p>© MTC Logistic, 2010–2024</p>
+        <div class="logo-footer-wrapper"><img class="logo-footer" src="<?php echo get_template_directory_uri() . '/assets/images/logo-white.png';?>" alt="MTC Logistic логотип"></div>
+        <p class="footer__privacy-policy">Политика конфиденциальности</p>
+    </div>
 </footer>
 
 <?php wp_footer();?>
--- functions.php
+++ functions.php
@@ -36,7 +36,9 @@ add_image_size('blog-small', 300, 200, true);
 function register_menus() {
     register_nav_menus(
         array(
-            'top-menu' => 'Header Menu'
+            'top-menu' => 'Header Menu',
+            'footer-menu' => 'Footer Menu',
+            'footer-menu-services' => 'Footer Menu Services'
         )
     );
 }
