ed6effab6787a9d4a3ab2679cfa70826461b7c94
Ilya <sange0337@gmail.com>
2024-08-11 23:07:15 +0800

    finished the second webform

--- includes/section-webform.php
+++ includes/section-webform.php
@@ -1,10 +1,8 @@
 <section class="webform-section" id="webform">
     <h2>Оставьте заявку на бесплатную консультацию</h2>
     <p class="form-subheading">мы свяжемся с вами в ближайшее время</p>
-        <form class="contact-webform" action="">
-            <input type="text" placeholder="Имя">
-            <input type="text" placeholder="Телефон">
-            <button class="submit-btn animated-btn-1">Заказать консультацию</button>
-        </form>
+        <div class="contact-webform">
+            <?php echo do_shortcode('[contact-form-7 id="bc48a79" title="Contact form 2"]');?>
+        </div>
     <p class="policy-consent">Нажимая на кнопку, вы соглашаетесь с <a href="<?php echo get_permalink_by_title("Privacy Policy"); ?>">политикой обработки данных</a></p>
 </section>
